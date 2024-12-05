10a593ff18dfa8af66337c16be011687d2fba5bc
BapanBigData <bapanmldl7892@gmail.com>
2024-12-05 17:18:44 +0530

    changes pushed

---
+++ test.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+
+print(torch.__version__)
+print(np.__version__)
+
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
