b9ccc491f216e790af3cf693b634aeff94de71a0
BapanBigData <bapanmldl7892@gmail.com>
2024-12-05 20:04:42 +0530

    changes pushed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="search_engine_model_training",
+    version="0.0.1",
+    author="Bapan Bairagya",
+    author_email="bapanmldl7892@gmail.com",
+    packages=find_packages()
+)
